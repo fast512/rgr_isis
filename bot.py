322726a3c18cb055ed9cab288b45a50a779a88d6
fast512 <zack229yoba@gmail.com>
2026-05-22 14:37:43 +1000

    упрощено

--- bot.py
+++ bot.py
@@ -6,11 +6,6 @@ from telegram.ext import Application, MessageHandler, filters, ContextTypes
 
 from config import BOT_TOKEN
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-
 MESSAGE = (
     'Система «Контроль версий ПО АСДУ ДПМ „Диалог“» разработана '
     'для управления версиями программного обеспечения в рамках '
@@ -29,8 +24,5 @@ def main():
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     
-    print("✅ Бот запущен!")
+    print("Бот запущен!")
     app.run_polling(allowed_updates=Update.ALL_TYPES)
-
-if __name__ == '__main__':
-    main()
